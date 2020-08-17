76c1ea75b490cad0f5f48452b5c08aa938c2c941
cpo007 <cpo007@qq.com>
2020-08-17 22:09:06 +0800

    20.8.17

--- TheAlgorithm3/TheAlgorithm3/StringExtension.swift
+++ TheAlgorithm3/TheAlgorithm3/StringExtension.swift
@@ -11,45 +11,85 @@ import Foundation
 extension String {
     /// String使用下标截取字符串
     /// string[index] 例如："abcdefg"[3] // c
-    subscript (i:Int)->String{
-        let startIndex = self.index(self.startIndex, offsetBy: i)
-        let endIndex = self.index(startIndex, offsetBy: 1)
-        return String(self[startIndex..<endIndex])
+    subscript(i: Int) -> String {
+        let startIndex = index(self.startIndex, offsetBy: i)
+        let endIndex = index(startIndex, offsetBy: 1)
+        return String(self[startIndex ..< endIndex])
     }
-    
-    
+
     /// String使用下标截取字符串
     /// string[index..<index] 例如："abcdefg"[3..<4] // d
-    subscript (r: Range<Int>) -> String {
-        get {
-            let startIndex = self.index(self.startIndex, offsetBy: r.lowerBound)
-            let endIndex = self.index(self.startIndex, offsetBy: r.upperBound)
-            return String(self[startIndex..<endIndex])
-        }
+    subscript(r: Range<Int>) -> String {
+        let startIndex = index(self.startIndex, offsetBy: r.lowerBound)
+        let endIndex = index(self.startIndex, offsetBy: r.upperBound)
+        return String(self[startIndex ..< endIndex])
     }
+
     /// String使用下标截取字符串
     /// string[index,length] 例如："abcdefg"[3,2] // de
-    subscript (index:Int , length:Int) -> String {
-        get {
-            let startIndex = self.index(self.startIndex, offsetBy: index)
-            let endIndex = self.index(startIndex, offsetBy: length)
-            return String(self[startIndex..<endIndex])
-        }
+    subscript(index: Int, length: Int) -> String {
+        let startIndex = self.index(self.startIndex, offsetBy: index)
+        let endIndex = self.index(startIndex, offsetBy: length)
+        return String(self[startIndex ..< endIndex])
     }
+
     // 截取 从头到i位置
-    func substring(to:Int) -> String{
-        return self[0..<to]
+    func substring(to: Int) -> String {
+        return self[0 ..< to]
     }
+
     // 截取 从i到尾部
-    func substring(from:Int) -> String{
-        return self[from..<self.count]
+    func substring(from: Int) -> String {
+        return self[from ..< count]
     }
 }
 
-
-//func printNodeVal(node: TreeNode?) {
+// func printNodeVal(node: TreeNode?) {
 //    guard let node = node else { return }
 //    print(node.val)
 //    printNodeVal(node: node.left)
 //    printNodeVal(node: node.right)
-//}
+// }
+
+public class TreeNode {
+    public var val: Int
+    public var left: TreeNode?
+    public var right: TreeNode?
+    
+    public var index: Int = 0
+    
+    public init(_ val: Int) {
+        self.val = val
+        left = nil
+        right = nil
+    }
+}
+
+var index = 0
+func quickSet(arr: [Any]) -> TreeNode? {
+    if index < arr.count - 1 && arr[index] is Int {
+        var arr = arr
+        let node = TreeNode(arr[index] as! Int)
+        index+=1
+        node.index = index
+        arr.removeFirst()
+        node.left = quickSet(arr: arr)
+        node.right = quickSet(arr: arr)
+        return node
+    } else {
+        index+=1
+        return nil
+    }
+}
+
+
+func quickPrint(node: TreeNode?) {
+    
+    guard let node = node else {
+        return
+    }
+    print(node.val)
+    quickPrint(node: node.left)
+    quickPrint(node: node.right)
+    
+}
--- TheAlgorithm3/TheAlgorithm3/main.swift
+++ TheAlgorithm3/TheAlgorithm3/main.swift
@@ -8,3 +8,19 @@
 
 import Foundation
 
+func isBalanced(_ root: TreeNode?) -> Bool {
+    return height(node: root) >= 1
+}
+
+public func height(node: TreeNode?) -> Int {
+    guard let node = node else {
+        return 0
+    }
+    let leftHeight = height(node: node.left)
+    let rightHeight = height(node: node.right)
+    if leftHeight == -1 || rightHeight == -1 || abs(leftHeight - rightHeight) > 1 {
+        return -1
+    } else {
+        return max(leftHeight, rightHeight) + 1
+    }
+}
